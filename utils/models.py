dd333e133aa7038c1c5457aeb111bb7fe20ac559
William Miller <77627423+miller-william@users.noreply.github.com>
2023-09-21 14:02:20 +0100

    Update models.py

--- utils/models.py
+++ utils/models.py
@@ -6,10 +6,10 @@ import tensorflow_hub as hub
 ########################### Model imports ###########################
 
 nlp = spacy.load("en_core_web_sm")
-#use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4") 
+use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4") 
 
 #model downloaded and saved locally because of issues using link above
-use_model = hub.load('/Users/william.miller/Downloads/universal-sentence-encoder_4')
+#use_model = hub.load('/Users/william.miller/Downloads/universal-sentence-encoder_4')
 
 #cross-encoder model
 cross_model = CrossEncoder('cross-encoder/ms-marco-TinyBERT-L-6')
